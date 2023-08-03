edf2a737154e9ba6411f79579a07523ce63ac67e
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-08-03 15:31:57 +0700

    fix: change footer space

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -8,7 +8,7 @@ const DefaultLayout = ({ children }: TProps) => {
   return (
     <>
       <Navbar />
-      <main className='mx-auto mb-40 max-w-xl space-y-8 px-7 pt-16 lg:px-0'>
+      <main className='mx-auto max-w-xl space-y-8 px-7 pb-40 pt-16 lg:px-0'>
         {children}
       </main>
     </>
