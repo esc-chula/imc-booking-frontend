deccc21c3f0743851615da6546e368b0228974fa
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-08-03 22:12:08 +0700

    chore: change component file name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Navbar from '@/components/Navbar';
-import Booking from '@/components/index/booking';
-import Faq from '@/components/index/faq';
-import Info from '@/components/index/info';
-import Instrument from '@/components/index/instrument';
-import Rule from '@/components/index/rule';
+import Booking from '@/components/index/Booking';
+import Faq from '@/components/index/Faq';
+import Info from '@/components/index/Info';
+import Instrument from '@/components/index/Instrument';
+import Rule from '@/components/index/Rule';
 import DefaultLayout from '@/layouts/DefaultLayout';
 
 const Index = () => {
