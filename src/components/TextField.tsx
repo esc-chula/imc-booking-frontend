edbf0e9f4c90f06fcb67853a1e130e76899e92b9
thanapatk <22460@tun.ac.th>
2023-08-30 07:50:47 +0700

    feat: add field error

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,6 +1,8 @@
+import clsx from 'clsx';
 import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
 
 type CustomTextFieldProps = {
+  error?: string;
   id: string;
   label?: string;
   placeholder?: string;
@@ -9,7 +11,13 @@ type CustomTextFieldProps = {
 type TextFieldProps = CustomTextFieldProps &
   DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
-const TextField = ({ id, label, placeholder, ...props }: TextFieldProps) => {
+const TextField = ({
+  id,
+  label,
+  placeholder,
+  error,
+  ...props
+}: TextFieldProps) => {
   return (
     <div className='w-full'>
       {label ? (
@@ -18,13 +26,20 @@ const TextField = ({ id, label, placeholder, ...props }: TextFieldProps) => {
         </label>
       ) : null}
       <input
-        className='w-full rounded-2xl border border-white/30 bg-darkgray py-4 pl-[1.125rem] pr-6 text-white placeholder:text-white placeholder:opacity-30'
+        className={clsx(
+          'w-full rounded-2xl border bg-darkgray py-4 pl-[1.125rem] pr-6 text-white placeholder:text-white placeholder:opacity-30',
+          {
+            'border-red': error,
+            'border-white/30': !error,
+          }
+        )}
         type='text'
         name={id}
         id={id}
         placeholder={placeholder}
         {...props}
       />
+      {error && <span className='mt-2 block text-red'>{error}</span>}
     </div>
   );
 };
